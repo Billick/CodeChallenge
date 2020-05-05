cba3c6b7769e14193a321341eded0e5978225286
Billick <64814107+Billick@users.noreply.github.com>
2020-05-04 22:02:49 -0400

    initial

---
+++ code-challenge/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenge.Models
+{
+    public class Employee
+    {
+        public String EmployeeId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Position { get; set; }
+        public String Department { get; set; }
+        public List<Employee> DirectReports { get; set; }
+    }
+
+    public class ReportingStructure
+    {
+        public String Employee { get; set; }
+        public int NumberOfReports { get; set; }
+    }
+}
